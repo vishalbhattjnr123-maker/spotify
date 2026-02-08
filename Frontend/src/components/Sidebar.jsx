123a8acb3b689fb8e01b402e31cbb3ad256e76d4
Vishal Bhatt <vishalbhattjnr123@gmail.com>
2026-02-08 16:46:37 +0530

    fixed urls

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const API_URL = "https://spotify-v5ue.onrender.com/";
+const API_URL = "https://spotify-v5ue.onrender.com";
 
 function Sidebar() {
   const [artists, setArtists] = useState([]);
